d4e12454078d2bc96be5a7059210cf38707bec00
szewah <swgrleong@gmail.com>
2019-06-02 12:39:45 -0400

    Updates

---
+++ precache-manifest.db2a27d5ed55c56ef1aa8e1316c34593.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22dc24af23ecd4fca3548bfc3846fae5",
+    "url": "/szewah/clickgame/index.html"
+  },
+  {
+    "revision": "98a36d84d70463025811",
+    "url": "/szewah/clickgame/static/css/main.0194af64.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/szewah/clickgame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "98a36d84d70463025811",
+    "url": "/szewah/clickgame/static/js/main.67da468d.chunk.js"
+  },
+  {
+    "revision": "a5559b7ad050333c7ec6",
+    "url": "/szewah/clickgame/static/js/runtime~main.3e9a45ac.js"
+  },
+  {
+    "revision": "4d5e65a0c53ec380d408bfde1d8ff703",
+    "url": "/szewah/clickgame/static/media/beth.4d5e65a0.png"
+  },
+  {
+    "revision": "612ba8d4e89b5cbe07b279808234f834",
+    "url": "/szewah/clickgame/static/media/birdperson.612ba8d4.png"
+  },
+  {
+    "revision": "0657e1f4c5b5eb7d25b4bd18811256a3",
+    "url": "/szewah/clickgame/static/media/evilmorty.0657e1f4.png"
+  },
+  {
+    "revision": "978b38471c32626814803817d75f4ab8",
+    "url": "/szewah/clickgame/static/media/gianthead.978b3847.png"
+  },
+  {
+    "revision": "3774b39dcbda08977c789663f6049c70",
+    "url": "/szewah/clickgame/static/media/goldenford.3774b39d.png"
+  },
+  {
+    "revision": "af8e8a84f14297f7c14073059a4b9246",
+    "url": "/szewah/clickgame/static/media/jerry.af8e8a84.png"
+  },
+  {
+    "revision": "a9644830b29212e0af6fb0a353c1c46d",
+    "url": "/szewah/clickgame/static/media/jessica.a9644830.png"
+  },
+  {
+    "revision": "9667b7eeed7cbde4439b6b6bba4a040f",
+    "url": "/szewah/clickgame/static/media/meeseeks.9667b7ee.png"
+  },
+  {
+    "revision": "244deeda3690049cebc0046457a6e1cf",
+    "url": "/szewah/clickgame/static/media/morty.244deeda.png"
+  },
+  {
+    "revision": "d61b2d5bd91638427b6604f6f7dcf1dc",
+    "url": "/szewah/clickgame/static/media/mr.d61b2d5b.png"
+  },
+  {
+    "revision": "06b6584198e9fce2075bc1a4a9bfae92",
+    "url": "/szewah/clickgame/static/media/rick.06b65841.png"
+  },
+  {
+    "revision": "afdbb7bfad6a6acefaafbf9fb072f0cb",
+    "url": "/szewah/clickgame/static/media/summer.afdbb7bf.png"
+  }
+]);
